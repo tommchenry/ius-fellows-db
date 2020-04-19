9e149f7f1568d2f995bd9da8bae33f7f584b3cec
Tom McHenry <tommchenry@gmail.com>
2020-04-19 18:09:27 -0500

    Make fellow optional, add admin page

---
+++ app/admin/deposits.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Deposit do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :deposit_number, :date_processed, :check_number, :fellow_id, :fellow_number, :check_date, :payment_by, :payment_type, :deposit_type, :gift_amount_cents, :gift_amount_currency, :period, :amount_cents, :amount_currency, :last_name
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:deposit_number, :date_processed, :check_number, :fellow_id, :fellow_number, :check_date, :payment_by, :payment_type, :deposit_type, :gift_amount_cents, :gift_amount_currency, :period, :amount_cents, :amount_currency, :last_name]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
